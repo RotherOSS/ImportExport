375a6b4b89ddd0d456cf5e2ba4fd0ed4678baa4f
bernhard <Bernhard.Schmalhofer@gmx.de>
2023-08-24 12:14:02 +0200

    Remove obsolet code for CodeUpgrade()

--- var/packagesetup/ImportExport.pm
+++ var/packagesetup/ImportExport.pm
@@ -47,8 +47,7 @@ sub new {
     my ( $Type, %Param ) = @_;
 
     # allocate new hash for object
-    my $Self = {};
-    bless( $Self, $Type );
+    my $Self = bless {}, $Type;
 
     # always discard the config object before package code is executed,
     # to make sure that the config object will be created newly, so that it
@@ -102,40 +101,6 @@ sub CodeUpgrade {
     return 1;
 }
 
-=head2 CodeUpgradeFromBefore_2_0_3()
-
-This function is only executed if the installed module version is smaller than 2.0.3.
-
-    my $Result = $CodeObject->CodeUpgradeFromBefore_2_0_3();
-
-=cut
-
-sub CodeUpgradeFromBefore_2_0_3 {    ## no critic qw(OTOBO::RequireCamelCase)
-    my ( $Self, %Param ) = @_;
-
-    # fix a typo in the database
-    $Self->_FixDatabaseTypo();
-
-    return 1;
-}
-
-=head2 CodeUpgradeFromLowerThan_4_0_91()
-
-This function is only executed if the installed module version is smaller than 4.0.91.
-
-my $Result = $CodeObject->CodeUpgradeFromLowerThan_4_0_91();
-
-=cut
-
-sub CodeUpgradeFromLowerThan_4_0_91 {    ## no critic qw(OTOBO::RequireCamelCase)
-    my ( $Self, %Param ) = @_;
-
-    # change configurations to match the new module location.
-    $Self->_MigrateConfigs();
-
-    return 1;
-}
-
 =head2 CodeUninstall()
 
 run the code uninstall part
@@ -152,62 +117,6 @@ sub CodeUninstall {
 
 =head1 PRIVATE INTERFACE
 
-=head2 _FixDatabaseTypo()
-
-    my $Result = $CodeObject->_FixDatabaseTypo();
-
 =cut
-
-sub _FixDatabaseTypo {
-    my ( $Self, %Param ) = @_;
-
-    # fix the ColumnSeperator typo (correct is ColumnSeparator)
-    return if !$Kernel::OM->Get('Kernel::System::DB')->Do(
-        SQL => "UPDATE imexport_format "
-            . "SET data_key = 'ColumnSeparator' "
-            . "WHERE data_key = 'ColumnSeperator'",
-    );
-
-    return 1;
-}
-
-=head2 _MigrateConfigs()
-
-change configurations to match the new module location.
-
-    my $Result = $CodeObject->_MigrateConfigs();
-
-=cut
-
-sub _MigrateConfigs {
-
-    # create needed objects
-    my $ConfigObject    = $Kernel::OM->Get('Kernel::Config');
-    my $SysConfigObject = $Kernel::OM->Get('Kernel::System::SysConfig');
-
-    my @NewSettings;
-
-    # migrate NavBar modules
-    # get setting content for NavBar modules
-    my $Setting = $ConfigObject->Get('Frontend::NavigationModule');
-
-    # update module location
-    $Setting->{'AdminImportExport'}->{Module} = "Kernel::Output::HTML::NavBar::ModuleAdmin";
-
-    # Build new setting.
-    push @NewSettings, {
-        Name           => 'Frontend::NavigationModule###AdminImportExport',
-        EffectiveValue => $Setting->{'AdminImportExport'},
-    };
-
-    # Write new setting.
-    $SysConfigObject->SettingsSet(
-        UserID   => 1,
-        Comments => 'ImportExport - package setup function: _MigrateConfigs',
-        Settings => \@NewSettings,
-    );
-
-    return 1;
-}
 
 1;
